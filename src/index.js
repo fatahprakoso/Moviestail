ac4f4c50a5ddbf35c21f7701dc31a323b2e7c3a1
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-01-21 09:11:39 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 // eslint-disable-next-line no-unused-vars
 import * as bootstrap from 'bootstrap';
-import '../scss/min.scss';
+import './scss/min.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const jsconfig = {
-  // if NODE_ENV undefined, then assign mode to production
-  // add prod & dev script on package.json
   mode: process.env.NODE_ENV ?? 'production',
-  entry: './src/js/min.js',
+  entry: './src/index.js',
   output: {
     filename: 'min.js',
     path: `${__dirname}/public/script`,
