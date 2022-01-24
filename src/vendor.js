f1ec6dbb8c57fcdaf3d1c58f7ffb354d859cc51c
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-01-24 12:34:59 +0700

    update

--- src/vendor.js
+++ src/vendor.js
@@ -1,6 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-unused-vars */
 
-import { button } from 'bootstrap';
+import { carousel } from 'bootstrap';
 /**
  * bootstrap classes:
  * alert, button, carousel, collapse, dropdown, modal, offcanvas, popover,
