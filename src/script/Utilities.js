1727def7fa4a24043aa442f43fae3c33bec5f2e2
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-02-13 22:35:15 +0700

    update: card styling

--- src/script/Utilities.js
+++ src/script/Utilities.js
@@ -2,17 +2,21 @@ async function getCards(searchText) {
   const data = await fetch(`http://www.omdbapi.com/?apikey=b206be1f&s=${searchText}`)
     .then((r) => r.json())
     .catch((e) => console.log(e))
-    .finally(() => {
-      console.log('finally');
-    });
+    .finally(() => console.log('finally'));
 
   let cardsElement = '';
   data.Search.forEach((c) => {
     cardsElement += `
       <div class="card movie-card">
-        <img src="${c.Poster}" class="card-img-top" alt="..." />
+        <img
+          src="${c.Poster}"
+          class="card-img-top movie-card-img"
+          alt="N/A"
+        />
         <div class="card-body">
-          <h4 class="card-title">${c.Title}</h4>
+          <h4 class="card-title">
+            ${c.Title.length < 17 ? c.Title : `${c.Title.substring(0, 13)}...`}
+          </h4>
           <h6 class="card-subtitle mb-3">${c.Year}, ${c.Type}</h6>
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
@@ -23,7 +27,46 @@ async function getCards(searchText) {
   return cardsElement;
 }
 
-// console.log(getCards('batman').then((e) => e));
-getCards('batman').then((e) => {
-  console.log(e);
-});
+function displayCards() {
+  const searchTextField = document.getElementById('search-text-field');
+  const searchButton = document.getElementById('search-button');
+  const carouselContainer = document.querySelector('.carousel-header');
+  const searchForm = document.querySelector('.search-form');
+  const cards = document.getElementById('cards');
+  console.log(Boolean(searchButton));
+
+  if (searchTextField.value.length > 0) {
+    searchForm.classList.add('search-form-colapse');
+
+    if (searchButton || carouselContainer) {
+      carouselContainer.classList.add('carousel-container-colapse');
+      searchButton.classList.add('search-btn-colapse');
+      setTimeout(() => {
+        searchButton.remove();
+      }, 600);
+    }
+
+    setTimeout(() => {
+      carouselContainer.remove();
+    }, 1000);
+
+    setTimeout(() => {
+      const movieCards = document.querySelectorAll('.movie-card');
+      let counter = 0;
+      movieCards.forEach((card) => {
+        counter += 100;
+        setTimeout(() => {
+          card.classList.add('movie-card-transition');
+        }, counter);
+      });
+    }, 1000);
+
+    getCards(searchTextField.value).then((cardsElement) => {
+      cards.innerHTML = cardsElement;
+    });
+
+    searchTextField.value = '';
+  }
+}
+
+export { getCards, displayCards };
