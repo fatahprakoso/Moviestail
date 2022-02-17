78f23004835e13640d3394dec388a9d0267fb139
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-02-17 09:42:54 +0700

    add spinner

--- src/script/Utilities.js
+++ src/script/Utilities.js
@@ -1,7 +1,7 @@
-async function getCards(searchText) {
-  const data = await fetch(`http://www.omdbapi.com/?apikey=b206be1f&s=${searchText}`)
+async function getCards(searchText, page = 1) {
+  const data = await fetch(`http://www.omdbapi.com/?apikey=b206be1f&s=${searchText}&page=${page}`)
     .then((r) => r.json())
-    .catch((e) => console.log(e))
+    .catch((e) => console.error(e))
     .finally(() => console.log('finally'));
 
   let cardsElement = '';
@@ -11,7 +11,7 @@ async function getCards(searchText) {
         <img
           src="${c.Poster}"
           class="card-img-top movie-card-img"
-          alt="N/A"
+          alt=""
         />
         <div class="card-body">
           <h4 class="card-title">
@@ -27,13 +27,13 @@ async function getCards(searchText) {
   return cardsElement;
 }
 
+const cards = document.getElementById('cards');
+
 function displayCards() {
   const searchTextField = document.getElementById('search-text-field');
   const searchButton = document.getElementById('search-button');
   const carouselContainer = document.querySelector('.carousel-header');
   const searchForm = document.querySelector('.search-form');
-  const cards = document.getElementById('cards');
-  console.log(Boolean(searchButton));
 
   if (searchTextField.value.length > 0) {
     searchForm.classList.add('search-form-colapse');
--- src/script/index.js
+++ src/script/index.js
@@ -2,3 +2,19 @@
 
 import '../scss/min.scss';
 import './components/search-form';
+
+window.onscroll = () => {
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    if (document.getElementById('spinner') === null) {
+      console.log('bottom');
+      const spinner = document.createElement('div');
+      spinner.innerHTML = `
+      <div class="text-center" id="spinner" style="margin-bottom: 1rem">
+        <div class="spinner-border" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>`;
+      document.getElementById('main-content').appendChild(spinner);
+    }
+  }
+};
