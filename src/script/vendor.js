8fcef8c79b817e69af384b6f88d50a114ea5bd91
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2022-03-03 09:09:03 +0700

    gh-pages

--- src/script/vendor.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable no-unused-vars */
-
-import { carousel } from 'bootstrap';
-/**
- * bootstrap classes:
- * alert, button, carousel, collapse, dropdown, modal, offcanvas, popover,
- * scrollspy, tab, toast, tooltip;
- */
